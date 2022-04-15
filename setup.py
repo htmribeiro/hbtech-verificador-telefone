f20c19cb88a42a3f6528e6977918e04b94998918
Hamilton-hbtechcompany <htadeu@hotmail.com>
2022-04-14 22:44:52 -0300

    Set setup.py to version 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
     
 setup(
     name="hbtech-verificador-telefone",
-    version="0.0.1",
+    version="0.0.2",
     author="Hamilton Ribeiro",
     author_email="hamilton.ribeiro@hbtechcompany.onmicrosoft.com",
     description="Get info which city a phone number belong to",
